77bca4066e3db915b2b198d220ef0f7abf488b50
elgun87 <elgun105@gmail.com>
2021-07-13 15:47:10 -0400

    modfied for loop

--- src/test/java/project/PrintAllChars.java
+++ src/test/java/project/PrintAllChars.java
@@ -2,8 +2,12 @@ package project;
 
 public class PrintAllChars {
     public static void main(String[] args) {
-        for(int i = 0;i < "hello world".length();i++){
-            System.out.println("hello world".charAt(i));
+        printChars("hello");
+    }
+
+    public static void printChars(String str){
+        for(int i = 0;i < str.length();i++){
+            System.out.println(str.charAt(i));
         }
     }
 }
