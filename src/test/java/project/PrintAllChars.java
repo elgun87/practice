af17a0f8c9d0b491335be1aabc3e9076cd0a6f1b
elgun87 <elgun105@gmail.com>
2021-07-13 15:42:04 -0400

    added new files

---
+++ src/test/java/project/PrintAllChars.java
@@ -0,0 +1,9 @@
+package project;
+
+public class PrintAllChars {
+    public static void main(String[] args) {
+        for(int i = 0;i < "hello world".length();i++){
+            System.out.println("hello world".charAt(i));
+        }
+    }
+}
